bd89ab241d6eb5075bbb51288917d1737cca7967
Marcel Klehr <mklehr@gmx.net>
2015-08-09 23:43:17 +0200

    let there be :bulb:

---
+++ client.js
@@ -0,0 +1,41 @@
+var bindEditor = require('./methodDrawBinding')
+
+module.exports = setup
+module.exports.consumes = ['editor']
+module.exports.provides = []
+function setup(plugin, imports, register) {
+  var editor = imports.editor
+
+  editor.registerEditor('svg', function*() {
+    var editor = document.querySelector('#editor')
+    
+    var iframe = document.createElement('iframe')
+    iframe.setAttribute('src', '/static/hive-editor-svg/lib/Method-Draw/index.html')
+    
+    // Maximize editor
+    document.body.style['position'] = 'absolute'
+    document.body.style['bottom'] =
+    document.body.style['top'] =
+    document.body.style['left'] =
+    document.body.style['right'] = '0'
+    document.body.style['overflow'] = 'hidden'
+    document.querySelector('#editor').style['height'] = '100%'
+    iframe.style['width'] = '100%'
+    iframe.style['height'] = '100%'
+    iframe.setAttribute('border', '0')
+    
+    // load the editor
+    yield function(cb) {
+      iframe.onload = function() {
+        cb()
+      }
+      editor.appendChild(iframe)
+    }
+    
+    /* bind editor */
+    var editable = iframe.contentDocument.querySelector('#svgcontent')
+      , methodDraw = iframe.contentDocument.defaultView.methodDraw
+    return bindEditor(methodDraw, iframe.contentDocument)
+  })
+  register()
+}
---
+++ methodDrawBinding.js
@@ -0,0 +1,81 @@
+/**
+ * methodDraw binding for gulf documents
+ * Copyright (C) 2015 Marcel Klehr <mklehr@gmx.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+var gulf = require('gulf')
+  , domOT = require('dom-ot')
+  , MutationSummary = require('mutation-summary')
+ 
+module.exports = function(methodDraw, document) {
+  var doc = new gulf.EditableDocument(new gulf.MemoryAdapter, domOT)
+  var contenteditable = document.querySelector('#svgcontent')
+  doc._change = function(newcontent, changes) {
+    observer && observer.disconnect()
+    console.log(newcontent)
+    if(changes) {
+      contenteditable = document.querySelector('#svgcontent')
+      var ops = domOT.unpackOps(changes)
+      ops.forEach(function(op) {
+        op.apply(contenteditable, /*index:*/true)
+      })
+      observer && observer.reconnect()
+    }
+    else {
+      methodDraw.loadFromString(newcontent.outerHTML)
+      setTimeout(function() {
+        contenteditable = document.querySelector('#svgcontent') // Must be after loadFromString
+        domOT.adapters.mutationSummary.createIndex(contenteditable)
+        registerObserver(contenteditable)
+      },1000)
+    }
+  }
+
+  doc._collectChanges = function() {
+    // changes are automatically collected by MutationSummary
+  }
+  
+  registerObserver(contenteditable)
+  domOT.adapters.mutationSummary.createIndex(contenteditable)
+  
+  var observer
+  function registerObserver(contenteditable) {
+      observer = new MutationSummary({
+      rootNode: contenteditable, // (defaults to window.document)
+      oldPreviousSibling: true,
+      queries: [
+        { all: true}
+      ],
+      callback: onChange
+    })
+  }
+
+  function onChange(summaries) {
+    var ops = domOT.adapters.mutationSummary.import(summaries[0], contenteditable)
+    ops = ops.filter(function(op) {
+      // filter out changes to the root node
+      if(op.path) return !!op.path.length
+      else return true
+    })
+    if(!ops.length) return
+    console.log(ops)
+    doc.update(ops)
+    ops.forEach(function(op) {
+      op.apply(contenteditable, /*index:*/true, /*dry:*/true)
+    })
+  }
+  
+  return doc
+}
