0d5b441dc811d7a4d1f5d2ff55260c2207c9cb35
Marcel Klehr <mklehr@gmx.net>
2016-01-17 03:08:11 +0100

    Hack to make it work inside iframe together with vdom
    Problem: Iframes loose content when detached from document
    Solution: We add the iframe to the body where it's never removed
    and position it directly above #editor

--- client.js
+++ client.js
@@ -8,28 +8,48 @@ function setup(plugin, imports, register) {
     , ui = imports.ui
 
   editor.registerEditor('Method-Draw', 'image/svg+xml', 'An easy to use, full-featured SVG editor'
-  , function(el) {
+  , function(el, onClose) {
     var iframe = document.createElement('iframe')
     iframe.setAttribute('src', ui.baseURL+'/static/hive-editor-svg-method-draw/lib/Method-Draw/index.html')
 
     // Maximize editor
     el.style['height'] = '100%'
-    iframe.style['width'] = '100%'
-    iframe.style['height'] = '100%'
-    iframe.setAttribute('border', '0')
+    iframe.style['position'] = 'absolute'
+    iframe.style['border'] = 'none'
+    iframe.style['visibility'] = 'hidden'
 
     // load the editor
     return new Promise(function(resolve) {
       iframe.onload = function() {
         resolve()
       }
-      el.appendChild(iframe)
+      // we don't add it to #editor `el`, because vdom cannot handle iframes
+      document.body.appendChild(iframe)
     }).then(function() {
+      setImmediate(updateFramePosition)
       iframe.contentDocument.querySelector('#menu_bar').style['visibility'] = 'hidden'
 
       /* bind editor */
       var methodDraw = iframe.contentDocument.defaultView.methodDraw
       return Promise.resolve(bindEditor(methodDraw, iframe.contentDocument))
+    })
+
+    window.addEventListener('scroll', updateFramePosition)
+    window.addEventListener('resize', updateFramePosition)
+
+    function updateFramePosition() {
+      var rect = el.getBoundingClientRect()
+      iframe.style['top'] = rect.top+'px'
+      iframe.style['left'] = rect.left+'px'
+      iframe.style['height'] = rect.height+'px'
+      iframe.style['width'] = rect.width+'px'
+      iframe.style['visibility'] = 'visible'
+    }
+
+    onClose(_ => {
+      window.removeEventListener('scroll', updateFramePosition)
+      window.removeEventListener('resize', updateFramePosition)
+      document.body.removeChild(iframe)
     })
   })
   register()
