c6f2c27548e6af2c2948727d3a5279c8f64b01b7
Marcel Klehr <mklehr@gmx.net>
2016-05-05 13:09:40 +0200

    [legal] Fix license headers

--- methodDrawBinding.js
+++ methodDrawBinding.js
@@ -1,19 +1,18 @@
 /**
- * methodDraw binding for gulf documents
+ * gulf bindings for MethodDraw
  * Copyright (C) 2015 Marcel Klehr <mklehr@gmx.net>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the Mozilla Public License version 2
+ * as published by the Mozilla Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the Mozilla Public License
+ * along with this program.  If not, see <https://www.mozilla.org/en-US/MPL/2.0/>.
  */
 var gulf = require('gulf')
   , domOT = require('dom-ot')
